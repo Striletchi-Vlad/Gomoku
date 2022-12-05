164f4ec57e694c53b995f96dda9f034ee26f084c
VladStriletchi <vladstriletchi@gmail.com>
2022-12-05 18:13:07 +0200

    added preview

--- a11-917-Striletchi-Vlad/main.py
+++ a11-917-Striletchi-Vlad/main.py
@@ -2,12 +2,12 @@
 from GUI import GraphicalMenu
 from console_ui import ConsoleMenu
 from domain import Board
+if __name__ == '__main__':
+    my_board = Board(15)
+    my_console = GraphicalMenu(my_board)
+    my_console.start()
 
-my_board = Board(15)
-my_console = GraphicalMenu(my_board)
-my_console.start()
 
 
-
-#TODO only some very slight weight calibrations
-#TODO check for full board (tie)
+    #TODO only some very slight weight calibrations
+    #TODO check for full board (tie)
